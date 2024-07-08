45b1676fe1a60b09e5511bf218a5bb9efac4ddc4
Gautier Darchen <gautier.darchen@swile.co>
2024-07-08 11:58:16 +0200

    fix(search): fix scroll issue on modal on mobile

--- src/app/watch/components/search/Search.tsx
+++ src/app/watch/components/search/Search.tsx
@@ -4,16 +4,11 @@ import React, { ChangeEvent, FC, Fragment, useEffect, useState } from 'react'
 import { HiSearch } from 'react-icons/hi'
 import { IoClose } from 'react-icons/io5'
 import { useDebounce } from 'use-debounce'
+import { useEventListener } from 'usehooks-ts'
 
 import WatchResource from '@/app/watch/watchResource'
 import Spinner from '@/components/spinner/Spinner'
-import {
-  Dialog,
-  DialogPanel,
-  Input,
-  Transition,
-  TransitionChild,
-} from '@headlessui/react'
+import { Input } from '@headlessui/react'
 
 import { searchPageByQuery } from './actions/searchPageByQueryAction'
 import NoResult from './components/NoResult'
@@ -29,6 +24,23 @@ const Search: FC = () => {
     useState<WatchResource[]>()
 
   const [debouncedQuery] = useDebounce(query, 300)
+
+  // Prevent background scroll while modal is opened
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden'
+      return
+    }
+
+    document.body.style.overflow = 'auto'
+  }, [isOpen])
+
+  // Close modal on 'ESC'
+  useEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      closeModal()
+    }
+  })
 
   // Execute the search server action everytime the debouncedQuery changes
   useEffect(() => {
@@ -72,83 +84,69 @@ const Search: FC = () => {
       <SearchButton placeholder={PLACEHOLDER} onOpenModal={openModal} />
 
       {/* Modal */}
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-30" onClose={closeModal}>
-          <TransitionChild
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black/60 backdrop-blur" />
-          </TransitionChild>
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+          {/* Backdrop */}
+          <button
+            className="fixed inset-0 bg-black/50 backdrop-blur"
+            onClick={closeModal}
+          />
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="absolute left-1/2 top-[50px] flex w-[90%] -translate-x-1/2 items-center justify-center p-4 text-center md:top-[110px] xl:w-1/2">
-              <TransitionChild
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <DialogPanel className="w-full max-w-2xl overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all dark:bg-gray-800">
-                  {/* Search input */}
-                  <div className="flex w-full items-center justify-center">
-                    <div className="group flex w-full items-center justify-center rounded-lg border-none bg-white/5 px-3 py-1.5 text-base/6 text-gray-800 outline outline-blue-400 dark:text-white">
-                      <HiSearch className="mr-2 text-lg" />
-                      <Input
-                        name="search"
-                        type="text"
-                        autoFocus
-                        autoComplete="off"
-                        placeholder={PLACEHOLDER}
-                        value={query}
-                        onChange={onInputChange}
-                        className="w-full bg-transparent data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 focus:outline-none"
-                      />
+          <div className="absolute top-[50px] mx-auto w-[90%] max-w-3xl md:top-[110px]">
+            {/* Content */}
+            <div className="relative m-auto flex max-h-[80dvh] flex-col overflow-auto overscroll-contain rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none dark:bg-gray-800">
+              <div className="relative flex-auto p-6">
+                {/* Search input */}
+                <div className="flex w-full items-center justify-center">
+                  <div className="group flex w-full items-center justify-center rounded-lg border-none bg-white/5 px-3 py-1.5 text-base/6 text-gray-800 outline outline-blue-400 dark:text-white">
+                    <HiSearch className="mr-2 text-lg" />
+                    <Input
+                      name="search"
+                      type="text"
+                      autoFocus
+                      autoComplete="off"
+                      placeholder={PLACEHOLDER}
+                      value={query}
+                      onChange={onInputChange}
+                      className="w-full bg-transparent data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 focus:outline-none"
+                    />
 
-                      {isLoading && <Spinner className="[&>svg]:size-5" />}
-                      {!isLoading && debouncedQuery && (
-                        <button onClick={onClearInput}>
-                          <IoClose className="text-lg hover:text-primary" />
-                        </button>
-                      )}
-                    </div>
+                    {isLoading && <Spinner className="[&>svg]:size-5" />}
+                    {!isLoading && debouncedQuery && (
+                      <button onClick={onClearInput}>
+                        <IoClose className="text-lg hover:text-primary" />
+                      </button>
+                    )}
                   </div>
+                </div>
 
-                  {/* Watch resource results */}
-                  {watchResourceResults && watchResourceResults.length > 0 && (
-                    <div className="mt-6 grid grid-cols-1 gap-4 md:grid-cols-2">
-                      {watchResourceResults.map((res) => {
-                        return (
-                          <WatchResource
-                            key={res.id}
-                            resource={res}
-                            query={debouncedQuery}
-                          />
-                        )
-                      })}
-                    </div>
-                  )}
+                {/* Watch resource results */}
+                {watchResourceResults && watchResourceResults.length > 0 && (
+                  <div className="mt-6 grid grid-cols-1 gap-4 md:grid-cols-2">
+                    {watchResourceResults.map((res) => {
+                      return (
+                        <WatchResource
+                          key={res.id}
+                          resource={res}
+                          query={debouncedQuery}
+                          truncate={false}
+                        />
+                      )
+                    })}
+                  </div>
+                )}
 
-                  {/* No result placeholder */}
-                  {debouncedQuery &&
-                    !isLoading &&
-                    !(
-                      watchResourceResults && watchResourceResults.length > 0
-                    ) && <NoResult query={debouncedQuery} />}
-                </DialogPanel>
-              </TransitionChild>
+                {/* No result placeholder */}
+                {debouncedQuery &&
+                  !isLoading &&
+                  !(
+                    watchResourceResults && watchResourceResults.length > 0
+                  ) && <NoResult query={debouncedQuery} />}
+              </div>
             </div>
           </div>
-        </Dialog>
-      </Transition>
+        </div>
+      )}
     </>
   )
 }
