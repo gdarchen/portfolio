fc34f5d9f757729b6ced80ee67ef9b453f7d8550
Gautier Darchen <darchen.gautier@gmail.com>
2024-07-24 15:47:15 +0200

    fix(media-around): hover in blue color on dark mode

--- src/components/home/social-media-around/SocialMediaAround.tsx
+++ src/components/home/social-media-around/SocialMediaAround.tsx
@@ -12,7 +12,7 @@ const IconClickableWithAnimation: FC<Props> = ({ href, Icon }) => {
   return (
     <motion.div whileHover={{ y: -3, transition: { duration: 0.1 } }}>
       <a href={href} target="_blank" rel="noreferrer">
-        <Icon className="size-6 fill-current text-gray-500 hover:cursor-pointer hover:text-primary dark:text-gray-400" />
+        <Icon className="size-6 fill-current text-gray-500 hover:cursor-pointer hover:text-primary dark:text-gray-400 dark:hover:text-primary" />
       </a>
     </motion.div>
   )
@@ -64,7 +64,7 @@ export default function SocialMediaEmail() {
               rel="noreferrer"
               className="hover:no-underline"
             >
-              <span className="font-header tracking-wider text-gray-500 hover:cursor-pointer hover:text-primary dark:text-gray-400">
+              <span className="font-header tracking-wider text-gray-500 hover:cursor-pointer hover:text-primary dark:text-gray-400 dark:hover:text-primary">
                 darchen.gautier<span className="text-primary">@</span>gmail
                 <span className="text-primary">.</span>com
               </span>
