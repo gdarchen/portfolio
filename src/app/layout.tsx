2d18093c2b07c70531e875af11b43e0294fc8f3b
Gautier Darchen <darchen.gautier@gmail.com>
2024-12-04 15:19:56 +0100

    feat(csq): inject CSQ lite tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { FC, PropsWithChildren } from 'react'
 import { Metadata } from 'next'
+import Script from 'next/script'
 
 import Header from '@/components/header/Header'
 import ScreenSizeDetector from '@/components/screen-size-detector/ScreenSizeDetector'
@@ -47,6 +48,22 @@ export const metadata: Metadata = {
   },
 }
 
+const CSQLiteScript: FC = () => {
+  return (
+    <Script id="csq-lite">
+      {`(function (c, s, q, u, a, r, e) {
+          c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
+          c._hjSettings = { hjid: a };
+          r = s.getElementsByTagName('head')[0];
+          e = s.createElement('script');
+          e.async = true;
+          e.src = q + c._hjSettings.hjid + u;
+          r.appendChild(e);
+      })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 5230603);`}
+    </Script>
+  )
+}
+
 const RootLayout: FC<Props> = ({ children }) => {
   const isProd = process.env.NODE_ENV === 'production'
 
@@ -65,6 +82,7 @@ const RootLayout: FC<Props> = ({ children }) => {
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
+        <CSQLiteScript />
       </head>
       <body>
         <div className="relative min-h-screen w-full snap-mandatory bg-background-light dark:bg-background">
